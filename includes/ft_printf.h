977d307ceb478123fd060737bd9d7edaadd81f67
Dylan Slogrove <dslogrove@gmail.com>
2018-09-19 14:36:59 +0200

    Fixed gnl

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,1 +1,58 @@
-../ft_printf/ft_printf.h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/05 16:35:46 by dslogrov          #+#    #+#             */
+/*   Updated: 2018/08/14 16:36:20 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <libft.h>
+# include <stdarg.h>
+
+typedef enum	e_types
+{
+	INT,
+	CHAR,
+	SHORT_INT,
+	LONG_LONG_INT,
+	LONG_INT,
+	INTMAX_T,
+	SIZE_T
+}				t_types;
+
+typedef enum	e_printf_flags
+{
+	FORCE_STYLE = 1,
+	PAD_ZERO = 2,
+	LEFT_JUSTIFY = 4,
+	SIGN_PLUS = 8,
+	SIGN_SPACE = 16
+}				t_printf_flags;
+
+typedef struct	s_printf_args
+{
+	char			format;
+	va_list			*args;
+	unsigned char	flags;
+	long int		width;
+	long int		precision;
+}				t_printf_args;
+
+int				ft_vprintf_fd(int fd, const char *format, va_list args);
+int				ft_printf(const char *format, ...);
+int				ft_printf_fd(int fd, const char *format, ...);
+
+char			*ft_getstr_all(t_printf_args *args);
+long int		ft_putarg(t_printf_args *a, int fd);
+size_t			pad_start(t_printf_args *a, const long int len, int fd);
+size_t			pad_end(t_printf_args *a, const long int len, const char *str,
+	int fd);
+
+#endif
--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -1,1 +1,22 @@
-../get_next_line/get_next_line.h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/24 16:01:00 by dslogrov          #+#    #+#             */
+/*   Updated: 2018/07/31 10:53:26 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 11000
+# include <unistd.h>
+# include <stdlib.h>
+# include <libft.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
